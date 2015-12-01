edb63b87905a46bd236919b0d18eda4a794720de
D_fresh <tarena@zh.local>
2015-12-01 10:31:50 +0800

    asjda

---
+++ kkkk/kkkk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kkkk
+//
+//  Created by tarena on 15/12/1.
+//  Copyright © 2015年 zhanghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
